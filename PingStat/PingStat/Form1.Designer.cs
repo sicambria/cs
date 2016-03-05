23c94d9bd2ddd4f2837939e6c3279e3f0f7b7b8e
Gaspar Incze <inczegaspar@yahoo.com>
2016-03-05 20:49:10 +0100

    PingStat upload

---
+++ PingStat/PingStat/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace PingStat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer_ping = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox_dest = new System.Windows.Forms.TextBox();
+            this.btn_dest_ok = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_stop_start = new System.Windows.Forms.Button();
+            this.cb_pingfail_sound = new System.Windows.Forms.CheckBox();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 254);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(484, 22);
+            this.statusStrip1.TabIndex = 0;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(77, 17);
+            this.toolStripStatusLabel1.Text = "PingNET v1.0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 32);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(141, 150);
+            this.textBox1.TabIndex = 1;
+            // 
+            // timer_ping
+            // 
+            this.timer_ping.Enabled = true;
+            this.timer_ping.Interval = 5000;
+            this.timer_ping.Tick += new System.EventHandler(this.timer_ping_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "PingNET aktív";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(397, 218);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Tálcára tesz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(98, 191);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(36, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel1.Location = new System.Drawing.Point(167, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(305, 199);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // textBox_dest
+            // 
+            this.textBox_dest.Location = new System.Drawing.Point(20, 217);
+            this.textBox_dest.Name = "textBox_dest";
+            this.textBox_dest.Size = new System.Drawing.Size(141, 20);
+            this.textBox_dest.TabIndex = 5;
+            this.textBox_dest.Text = "hu.wikipedia.org";
+            // 
+            // btn_dest_ok
+            // 
+            this.btn_dest_ok.Location = new System.Drawing.Point(167, 217);
+            this.btn_dest_ok.Name = "btn_dest_ok";
+            this.btn_dest_ok.Size = new System.Drawing.Size(57, 24);
+            this.btn_dest_ok.TabIndex = 6;
+            this.btn_dest_ok.Text = "OK";
+            this.btn_dest_ok.UseVisualStyleBackColor = true;
+            this.btn_dest_ok.Click += new System.EventHandler(this.btn_dest_ok_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Célállomás nem volt elérhető:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "mp";
+            // 
+            // btn_stop_start
+            // 
+            this.btn_stop_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_stop_start.Location = new System.Drawing.Point(257, 218);
+            this.btn_stop_start.Name = "btn_stop_start";
+            this.btn_stop_start.Size = new System.Drawing.Size(108, 23);
+            this.btn_stop_start.TabIndex = 8;
+            this.btn_stop_start.Text = "STOP";
+            this.btn_stop_start.UseVisualStyleBackColor = true;
+            this.btn_stop_start.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cb_pingfail_sound
+            // 
+            this.cb_pingfail_sound.AutoSize = true;
+            this.cb_pingfail_sound.Location = new System.Drawing.Point(20, 192);
+            this.cb_pingfail_sound.Name = "cb_pingfail_sound";
+            this.cb_pingfail_sound.Size = new System.Drawing.Size(52, 17);
+            this.cb_pingfail_sound.TabIndex = 9;
+            this.cb_pingfail_sound.Text = "Hang";
+            this.cb_pingfail_sound.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 276);
+            this.Controls.Add(this.cb_pingfail_sound);
+            this.Controls.Add(this.btn_stop_start);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_dest_ok);
+            this.Controls.Add(this.textBox_dest);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "PingNET";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer_ping;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox_dest;
+        private System.Windows.Forms.Button btn_dest_ok;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_stop_start;
+        private System.Windows.Forms.CheckBox cb_pingfail_sound;
+    }
+}
+
---
+++ PingStat/PingStat/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.IO;
+
+namespace PingStat
+{
+    public partial class Form1 : Form
+    {
+        //---------------------------------------
+        string destination = "hu.wikipedia.org";
+        //---------------------------------------
+
+        int ping_time_ms = 0;
+        List<int> pinglist = new List<int>();
+        int iter = 1;
+        bool ping_failure = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void timer_ping_Tick(object sender, EventArgs e)
+        {
+            pingSite(destination);
+        }
+
+        void pingSite(string webpage)
+        {
+            System.Net.NetworkInformation.Ping pinger = new System.Net.NetworkInformation.Ping();
+            ping_time_ms = 0;
+
+            try
+            {
+                PingReply pingreply = pinger.Send(webpage);
+
+                string pinganswer = pingreply.Address + " - " + pingreply.Status.ToString() + " - " + pingreply.RoundtripTime.ToString() + "ms";
+
+
+                ping_time_ms = (int)pingreply.RoundtripTime;
+
+                if (pinglist.Count > 30)
+                {
+                    pinglist.Clear();
+                }
+
+                pinglist.Add(ping_time_ms);
+
+                drawping();
+
+                toolStripStatusLabel1.Text = pinganswer + " - "
+                    + DateTime.Now.ToString();
+
+                if (pingreply.Status != IPStatus.Success)
+                {
+                    textBox1.Text += DateTime.Now.ToString()
+                        + "\r\n";
+                    ping_failure = true; // log fájl mentése
+
+                    if (cb_pingfail_sound.Checked)
+                    {
+                        Console.Beep(2000, 500);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                toolStripStatusLabel1.Text = ex.Message;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hide();
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            if (ping_failure)
+            {
+                string timestamp = DateTime.Now.ToString().Replace(':', '-').Replace(".", "-");
+                TextWriter writer = new StreamWriter(Application.StartupPath + @"\"
+                    + timestamp + ".log");
+
+                writer.WriteLine(textBox1.Text);
+                writer.Close();
+            }
+
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            timer_ping.Interval = (int)numericUpDown1.Value * 1000;
+        }
+
+
+        void drawping()
+        {
+            panel1.Invalidate();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "PingNET - " + destination;
+            textBox_dest.Text = destination;
+        }
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen pen1 = new Pen(Color.Green, 4f);
+            Pen pen2 = new Pen(Color.Orange, 4f);
+            Pen pen3 = new Pen(Color.Red, 4f);
+
+            Pen pen_error = new Pen(Color.Black, 4f);
+
+            //g.DrawLine(pen1, 0, 0, this.Width, this.Height);
+            SolidBrush brushOne = new SolidBrush(Color.Green);
+
+
+            foreach (int pingtime in pinglist)
+            {
+                if (pingtime < 36)
+                {
+                    g.DrawLine(pen1, 0, iter * 6, (pingtime - 10) * 3, iter * 6);
+                }
+
+                if (pingtime >= 36 && pingtime < 60)
+                {
+                    g.DrawLine(pen2, 0, iter * 6, (pingtime - 10) * 3, iter * 6);
+                }
+
+                if (pingtime >= 60)
+                {
+                    g.DrawLine(pen3, 0, iter * 6, (pingtime - 10) * 3, iter * 6);
+                }
+
+                if (pingtime == 0)
+                {
+                    g.DrawLine(pen_error, 0, iter * 6, 300, iter * 6);
+                }
+
+                iter++;
+            }
+            iter = 1;
+        }
+
+        private void btn_dest_ok_Click(object sender, EventArgs e)
+        {
+            destination = textBox_dest.Text;
+            this.Text = "PingNET - " + destination;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (timer_ping.Enabled)
+            {
+                timer_ping.Enabled = false;
+                btn_stop_start.Text = "START";
+                notifyIcon1.Text = "PingNET leállítva";
+            }
+            else
+            {
+                timer_ping.Enabled = true;
+                btn_stop_start.Text = "STOP";
+                notifyIcon1.Text = "PingNET aktív";
+            }
+        }
+
+
+
+    }
+}
